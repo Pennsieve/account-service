3fa5baaed7bc2371f487e592ac6e1572cdb8e673
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2026-02-02 16:57:26 -0500

    add names to output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -25,3 +25,19 @@ output "compute_nodes_table_arn" {
 output "compute_nodes_access_table_arn" {
   value = aws_dynamodb_table.compute_node_access_table.arn
 }
+
+output "accounts_table_name" {
+  value = aws_dynamodb_table.accounts_table.name
+}
+
+output "accounts_workspace_table_name" {
+  value = aws_dynamodb_table.account_workspace_table.name
+}
+
+output "compute_nodes_table_name" {
+  value = aws_dynamodb_table.compute_resource_nodes_table.name
+}
+
+output "compute_nodes_access_table_name" {
+  value = aws_dynamodb_table.compute_node_access_table.name
+}
